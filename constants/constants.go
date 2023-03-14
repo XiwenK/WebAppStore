bb8cf05bcd23892be502d97f97e9cdd4d3c3f6c5
seanking <152>
2023-03-14 10:19:17 +0800

    first commit

---
+++ constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	APP_INDEX = "app"
+	USER_INDEX = "user"
+	ES_URL = "http://35.240.231.190:9200"
+	ES_USERNAME = "xiwenk188"
+	ES_PASSWORD = "xiaoxiao20001009"
+	STRIPE_API_KEY = "sk_test_51MdStGC4MWrAeuPvbeRHIo5kzmU98Ip8kRBKTEB096PLuKTHUjAYYMXzEqJC71ZOvkRkopb0F2Cz8lQhdLGovcU600iB7U34Qf"
+	GCS_BUCKET = "sean-appstore"
+)
